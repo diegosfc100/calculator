63323d0eeb07acc3610b7177f5a403b017baa549
DIEGO RODRIGUES <diego.sfc100@hotmail.com>
2021-02-25 12:18:43 -0300

    update CE function display

--- script.js
+++ script.js
@@ -1,40 +1,47 @@
 display = document.querySelector("input");
 
-let list = []; //limit to size 12
-let current_operation = "";
-let cancelThis = "";
+let list = []; //values displayed
+let current_operation = ""; //values calculated
+
 
 function myFunction(x) {
-   
+
+
+  //Calculate and display answer after '=' button is pressed
   function checkVal() {
     console.log(current_operation);
     try{
-      
+
         let answer = eval(current_operation);
     // display.value = answer.toPrecision(4);
         display.value = answer
         if (list.length === 13 && !(isNaN(list[list.length-1]))) {
             list = [];
             list.push(answer);
+            current_operation = answer.toString();
+            console.log(current_operation)
         }else {
             list = [];
             list.push(answer);
+            current_operation = answer.toString();
+            console.log(current_operation)
           }
     }catch(e){
          display.value = 'ERROR'
        }
   }
-  
-  
+
+    //When AC button's pressed
   function allCancel() {
     current_operation = "";
     display.value = "0";
     list = [];
   }
-  
-  
-  
+
+
+  //When CE button's pressed
   function cancelEntry() {
+    list.pop()
     let number = current_operation;
     let splitter = "";
 
@@ -48,33 +55,39 @@ function myFunction(x) {
       }
     }
     let newdisplay = number.split(splitter);
+    list.join('')
+    list.split(splitter)
+    list.pop()
     newdisplay.pop();
     current_operation = newdisplay.join("");
-    list = newdisplay;
-    display.value = current_operation;
+//     list = newdisplay;
+//     display.value = current_operation;
+    display.value = list.join('')
   }
-
+  //builds current_operation/calls checkVal() when '=' is pressed/calls AC and CE functions
   if (x) {
+    console.log(x)
     if (x === "=") {
       checkVal();
-    } 
+    }
     else {
       list.push(x);
       display.value = list.join("");
       if (x === "x") {
         current_operation += "*";
+        console.log(current_operation)
       } else if (x === "÷") {
         current_operation += "/";
+        console.log(current_operation)
       } else if (x === "AC") {
         allCancel();
       } else if (x === "CE") {
         cancelEntry();
       } else {
         current_operation += x;
+        console.log(current_operation)
       }
     }
   }
-//   if (!(isNaN(list[list.length-1]))){
-//     checkVal();}
 
 }
